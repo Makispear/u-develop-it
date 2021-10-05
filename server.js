91356a0dd63749acddb7a4b2275dacd6ce893d64
Maki Aboabida <maki-miko@hotmail.com>
2021-10-05 17:15:34 -0400

    add: setup express npm

---
+++ server.js
@@ -0,0 +1,22 @@
+// EXPRESS
+const express = require('express')
+const PORT = process.env.PORT || 3001
+const app = express()
+// EXPRESS MIDDLEWARE 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+// GET 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    })
+})
+
+app.use((req, res) => {
+    res.status(400).end()
+})
+
+// LISTEN 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
