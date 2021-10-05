e3fc19769240529e21b47825960fa5612401a524
Maki Aboabida <maki-miko@hotmail.com>
2021-10-05 17:30:39 -0400

    add: connect SQL to server

--- server.js
+++ server.js
@@ -2,9 +2,20 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
+// SQL 
+const mysql = require('mysql2')
 // EXPRESS MIDDLEWARE 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+// CONNECT TO DATABASE
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Maki@1951',
+    database: 'election'
+    },
+    console.log('Connected to the sql database.')
+)
 // GET 
 app.get('/', (req, res) => {
     res.json({
