2e7b52b2d6477a0079b5c4e9a1440a8689859437
Maki Aboabida <maki-miko@hotmail.com>
2021-10-07 03:10:01 -0400

    Bug: Source command not running

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Maki@1951',
+    database: 'election'
+    },
+    console.log('Connected to the sql database.')
+)
+
+module.exports = db;
